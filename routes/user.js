fc6f221bc2c75f082f66d2c399bf2e77c0052480
Sanket Khardekar <sanket.khardekar@mindbowser.com>
2022-05-27 14:07:25 +0530

    Added Password Hashing

--- routes/user.js
+++ routes/user.js
@@ -2,24 +2,25 @@ const express = require("express");
 const router = express.Router();
 const { v4: uuidv4}=require('uuid');
 const validator = require("../utils/validator.js");
-const user = require("../middleware/user.js");
+const bcrypt=require('bcrypt');
 const db = require("../services/connection");
 //Signup Route
 router.post("/signup", validator.validSignupData, (req, res) => {
   let query = `SELECT * FROM user WHERE email='${req.body.email}'`;
-  db.query(query, (err, result) => {
+  db.query(query, async(err, result) => {
     if (!err) {
       if (result.length > 0) {
         res.status(409).json({message:"Email Already Exists"});
         return;
       } else {
+        const hashedPassword=await bcrypt.hash(req.body.password,10);
         const newId = uuidv4()
         let user = {
           id: newId,
           email: req.body.email,
           name: req.body.name,
           phone: req.body.phone,
-          password: req.body.password,
+          password: await hashedPassword,
           status:1,
         };
         query = `INSERT INTO user SET ?`;
@@ -28,6 +29,7 @@ router.post("/signup", validator.validSignupData, (req, res) => {
             res.status(200).json({message:"Registration Successfull", userId: newId});
             return;
           } else {
+            console.log(err); 
             res.status(409).json({message:"Registration Failed Try Again Later"});
             return;
           }
@@ -41,24 +43,24 @@ router.post("/signup", validator.validSignupData, (req, res) => {
 });
 
 //Login Route
-router.post("/login", validator.validLoginData, (req, res) => {
+router.post("/login", validator.validLoginData,  (req, res) => {
   const query = `SELECT * FROM user WHERE email='${req.body.email}'AND status=1`;
-  db.query(query, (err, result) => {
+  db.query(query, async (err, result) => {
     if (!err) {
       if (result.length > 0) {
-        if (result[0].password === req.body.password) {
-          res.status(200).send({message:"Login Sucessfull", userId:result[0].id});
+        if (await bcrypt.compare(req.body.password,result[0].password)) {
+          res.status(200).json({message:"Login Sucessfull", userId:result[0].id});
           return;
         } else {
-          res.status(401).send("Wrong Password");
+          res.status(401).json({message:"Wrong Password"});
           return;
         }
       } else {
-        res.status(401).send("Email Id Not Found!");
+        res.status(401).json({message:"Email Id Not Found!"});
         return;
       }
     } else {
-      res.status(409).send("Login Failed Try Again Later");
+      res.status(409).json({message:"Login Failed Try Again Later"});
       return;
     }
   });
@@ -97,23 +99,23 @@ router
     db.query(query, (err, result) => {
       if (!err) {
         if (result.length <= 0) {
-          res.status(409).send("User Does not Exists");
+          res.status(409).json({message:"User Does not Exists"});
           return;
         } else {
             let updateData=req.body;
           query = `UPDATE user SET ? WHERE id=?`;
           db.query(query,[updateData,req.query.id],(err, result) => {
             if (!err) {
-              res.status(200).send("User Updated Successfull");
+              res.status(200).json({message:"User Updated Successfull"});
               return;
             } else {
-              res.status(409).send("Update Failed Try Again Later");
+              res.status(409).json({message:"Update Failed Try Again Later"});
               return;
             }
           });
         }
       } else {
-        res.status(409).send("Update Failed Try Again Later");
+        res.status(409).json({message:"Update Failed Try Again Later"});
         return;
       }
     });
@@ -121,7 +123,7 @@ router
 
 //404 Page Not Found Route
 router.all("*", (req, res) => {
-  res.status(404).send("Page Not Found!");
+  res.status(404).json({message:"Page Not Found!"});
 });
 
 module.exports = router;
